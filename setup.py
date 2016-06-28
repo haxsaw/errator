1911f7cf743e933b9b67b25d0dc82672350a5d80
haxsaw <tcarroll@pobox.com>
2016-06-28 12:26:07 +0100

    initial steps to getting on pypi

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+from errator import __version__
+
+setup(
+    name="errator",
+    py_modules=["errator"],
+    version=__version__,
+    description="Errator allows you to create human-readable exception narrations",
+    author="Tom Carroll",
+    author_email="actuator@pobox.com",
+    url="https://github.com/haxsaw/errator",
+    download_url="https://github.com/haxsaw/errator/tarball/0.1",
+    keywords=["exception", "logging", "traceback", "stacktrace"],
+    classifiers=[],
+)
